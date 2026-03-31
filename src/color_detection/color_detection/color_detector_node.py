f0a9d608be1a7e4c7287951cf8ce90b49ffb551d
e-cagan <emincaganapaydin@gmail.com>
2026-03-31 18:30:47 +0300

    first commit

---
+++ src/color_detection/color_detection/color_detector_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from color_detection.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/pick_and_place/pick_and_place/pick_place_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from pick_and_place.')
+
+
+if __name__ == '__main__':
+    main()
